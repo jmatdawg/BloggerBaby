ef3dc908bb720afb6e06f48c15574179d956eb84
jmatdawg <matthews_jakiera@yahoo.com>
2018-03-13 14:19:58 -0500

    stripe is working, mailgun next

---
+++ server/src/utils/mail.js
@@ -0,0 +1,14 @@
+import mailgunLoader from 'mailgun-js';
+let mailgun = mailgunLoader({ 
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: 'sandbox12d795ff5fa749bcb116d8b9eac45ec4.mailgun.org',
+})
+
+function sendEmail(to, from, subject, content) {
+    let data = {
+        from,
+        to,
+        subject,
+        html
+    }
+}
